0e049fb0ed7da592b962965f244120df86024c31
tahscenery <tahscenery@gmail.com>
2019-10-16 00:15:37 +1100

    Initial Commit

---
+++ Dash/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  Dash
+//
+//  Created by Ta-Seen Islam on 16/10/19.
+//  Copyright © 2019 Ta-Seen Islam. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
