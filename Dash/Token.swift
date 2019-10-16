f9d0abd05073efa588a7ea90121d21cb2fbbd762
tahscenery <tahscenery@gmail.com>
2019-10-16 17:56:32 +1100

    Refactoring

--- Dash/Token.swift
+++ Dash/Token.swift
@@ -8,7 +8,16 @@
 
 import Foundation
 
-typealias Literal = Any
+typealias LiteralType = Any
+
+enum Keyword {
+    case and, `class`, dbg, `else`, `false`, fun, `for`, `if`, nothing, or, `return`, `self`, `super`, then,
+        `true`, `var`, `while`
+}
+
+enum Literal {
+    case identifier, number, string
+}
 
 enum TokenType {
     /// Single-character tokens
@@ -18,24 +27,45 @@ enum TokenType {
     case bang, bangEqual, equal, equalEqual, greater, greaterEqual, less, lessEqual
     
     /// Keywords
-    case and, `class`, `else`, `false`, fun, `for`, `if`, nothing, or, print, `return`, `self`, `super`,
-        `true`, `var`, `while`
+    case keyword(Keyword)
     
     /// Literals
-    case identifier, number, string
+    case literal(Literal)
     
     /// End of file
     case eof
+    
+    static func keyword(fromString string: String) -> TokenType? {
+        switch string {
+        case "and":     return .keyword(.and)
+        case "class":   return .keyword(.class)
+        case "dbg":     return .keyword(.dbg)
+        case "else":    return .keyword(.else)
+        case "false":   return .keyword(.false)
+        case "for":     return .keyword(.for)
+        case "fun":     return .keyword(.fun)
+        case "if":      return .keyword(.if)
+        case "nothing": return .keyword(.nothing)
+        case "or":      return .keyword(.or)
+        case "return":  return .keyword(.return)
+        case "self":    return .keyword(.`self`)
+        case "super":   return .keyword(.super)
+        case "true":    return .keyword(.true)
+        case "var":     return .keyword(.var)
+        case "while":   return .keyword(.while)
+        default:        return nil
+        }
+    }
 }
 
 class Token {
     
     let type: TokenType
     let lexeme: String
-    let literal: Literal?
+    let literal: LiteralType?
     let line: Int
     
-    init(withType type: TokenType, lexeme: String, literal: Literal?, line: Int) {
+    init(withType type: TokenType, lexeme: String, literal: LiteralType?, line: Int) {
         self.type = type
         self.lexeme = lexeme
         self.literal = literal
@@ -46,6 +76,12 @@ class Token {
 
 extension Token: CustomStringConvertible {
     
-    var description: String { "Token(type: \(self.type), lexeme: '\(self.lexeme)', literal: '\(self.literal ?? "<no-literal>")')" }
+    var description: String {
+        if let literal = self.literal {
+            return "Token(type: \(self.type), lexeme: '\(self.lexeme)', literal: \(literal))"
+        } else {
+            return "Token(type: \(self.type), lexeme: '\(self.lexeme)')"
+        }
+    }
     
 }
